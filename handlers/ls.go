61520b7dc961610c3328df649587640008fcb142
headzoo <sean@headzoo.io>
2014-08-11 01:54:27 -0400

    Updated handlers.ls.go
    Changed how the column widths are determined

--- handlers/ls.go
+++ handlers/ls.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"path"
+	"strconv"
 
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/headzoo/etcdsh/io"
@@ -15,6 +16,13 @@ const (
 	// Represents a node is a file in the output.
 	typeObject = "o"
 )
+
+// Column widths to use for the "ls" output.
+type ColumnWidths struct {
+	CreatedIndex  int
+	ModifiedIndex int
+	TTL           int
+}
 
 // LsHandler handles the "ls" command.
 type LsHandler struct {
@@ -57,35 +65,87 @@ func (h *LsHandler) Handle(i *io.Input) (string, error) {
 		return "", err
 	}
 
-	total := 2
+	widths := columnWidths(resp.Node)
 	node := etcd.Node{
 		Dir:           true,
 		Key:           ".",
 		CreatedIndex:  0,
 		ModifiedIndex: 0,
 	}
-	output := formatNode(&node)
+	output := formatNode(&node, widths)
 	node.Key = ".."
-	output += formatNode(&node)
+	output += formatNode(&node, widths)
 
+	total := 2
 	for _, node := range resp.Node.Nodes {
-		output += formatNode(node)
+		output += formatNode(node, widths)
 		total++
 		for _, n := range node.Nodes {
-			output += formatNode(n)
+			output += formatNode(n, widths)
 			total++
 		}
 	}
 
-	output = fmt.Sprintf("total %d\n%s", total, output)
-	return output, nil
+	return fmt.Sprintf("total %d\n%s", total, output), nil
 }
 
 // formatNode formats the node as a string for output to the console.
-func formatNode(n *etcd.Node) string {
-	t := typeKey
+func formatNode(n *etcd.Node, w ColumnWidths) string {
+	typeValue := typeKey
 	if !n.Dir {
-		t = typeObject
+		typeValue = typeObject
 	}
-	return fmt.Sprintf("%-3d%-3d%-3d%s %s\n", n.CreatedIndex, n.ModifiedIndex, n.TTL, t, path.Base(n.Key))
+
+	return fmt.Sprintf(
+		"%*d %*d %*d %s: %s\n",
+		w.CreatedIndex,
+		n.CreatedIndex,
+		w.ModifiedIndex,
+		n.ModifiedIndex,
+		w.TTL,
+		n.TTL,
+		typeValue,
+		path.Base(n.Key),
+	)
+}
+
+// columnWidths returns the widths for each column in the "ls" output.
+func columnWidths(resp_node *etcd.Node) ColumnWidths {
+	widths := ColumnWidths{
+		CreatedIndex:  len(strconv.FormatUint(resp_node.CreatedIndex, 10)),
+		ModifiedIndex: len(strconv.FormatUint(resp_node.ModifiedIndex, 10)),
+		TTL:           len(strconv.FormatInt(resp_node.TTL, 10)),
+	}
+	cw := 0
+
+	for _, node := range resp_node.Nodes {
+		cw = len(strconv.FormatUint(node.CreatedIndex, 10))
+		if cw > widths.CreatedIndex {
+			widths.CreatedIndex = cw
+		}
+		cw = len(strconv.FormatUint(node.ModifiedIndex, 10))
+		if cw > widths.ModifiedIndex {
+			widths.ModifiedIndex = cw
+		}
+		cw = len(strconv.FormatInt(node.TTL, 10))
+		if cw > widths.TTL {
+			widths.TTL = cw
+		}
+		for _, n := range node.Nodes {
+			cw = len(strconv.FormatUint(n.CreatedIndex, 10))
+			if cw > widths.CreatedIndex {
+				widths.CreatedIndex = cw
+			}
+			cw = len(strconv.FormatUint(n.ModifiedIndex, 10))
+			if cw > widths.ModifiedIndex {
+				widths.ModifiedIndex = cw
+			}
+			cw = len(strconv.FormatInt(n.TTL, 10))
+			if cw > widths.TTL {
+				widths.TTL = cw
+			}
+		}
+	}
+
+	return widths
 }
