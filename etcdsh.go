074fc415dcd1a720434330826e30a95836629ad8
headzoo <sean@headzoo.io>
2014-08-13 14:47:19 -0400

    Formatting

--- etcdsh.go
+++ etcdsh.go
@@ -13,13 +13,13 @@ import (
 // Main method.
 func main() {
 	config := config.New()
-
+	
 	help := flag.Bool("help", false, "Prints command line options and exit.")
 	version := flag.Bool("version", false, "Prints the etcdsh version and exit.")
 	flag.StringVar(&config.Machine, "machine", config.Machine, "Connect to this etcd server.")
 	flag.BoolVar(&config.Colors, "colors", config.Colors, "Use colors in display.")
 	flag.Parse()
-
+	
 	if *help {
 		printHelp()
 		os.Exit(0)
